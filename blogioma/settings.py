7d2e90560767688d5895b79c83bdd5ad73e57547
tiomchik <artemkaun228@gmail.com>
2023-09-03 15:14:00 +0300

    changed language code and time zone

--- blogioma/settings.py
+++ blogioma/settings.py
@@ -99,9 +99,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "ru-RU"
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "Europe/Moscow"
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
