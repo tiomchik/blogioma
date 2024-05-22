ee0e5cc53b2ecf2a3cc65b8db7236817bf0cd54b
tiomchik <artemkaun228@gmail.com>
2024-05-22 09:42:39 +0300

    Innovation / writted test for random-article API view

--- api/tests/test_articles.py
+++ api/tests/test_articles.py
@@ -121,6 +121,18 @@ class ArticleTests(GenericTestCase):
 
         self._response_contains_article(r, data)
 
+    def test_random(self) -> None:
+        for i in range(10):
+            self._create_article(headling=f"test_article{i}")
+
+        url = reverse("article-random-article")
+        r = self.client.get(url)
+
+        self.assertEqual(r.status_code, status.HTTP_200_OK)
+        self.assertIsNotNone(r.json().get("headling"))
+        self.assertIsNotNone(r.json().get("full_text"))
+        self.assertIsNotNone(r.json().get("author"))
+
     # ====================
     # ====== Update ======
     # ====================
