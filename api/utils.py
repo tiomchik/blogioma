9a4162e9fda1d02714b56cb8ec0a53a046117f30
tiomchik <artemkaun228@gmail.com>
2024-05-22 08:50:50 +0300

    Innovation / writted random-article API view and added plus_viewing function to api/utils.py

--- api/utils.py
+++ api/utils.py
@@ -1,7 +1,18 @@
+from django.db.models import F
 from rest_framework.pagination import PageNumberPagination
+
+from articles.models import Article
 
 
 class Pagination(PageNumberPagination):
     page_size = 50
     page_size_query_param = "page_size"
     max_page_size = 100
+
+
+def plus_viewing(article: Article) -> None:
+    """Changes article's `viewings` field to += 1 and saves passed
+    instance."""
+    article.viewings = F("viewings") + 1
+    article.save()
+    article.refresh_from_db()
