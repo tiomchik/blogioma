9ea90321979e7141a9d5062be207fe50610dbca1
tiomchik <artemkaun228@gmail.com>
2024-06-29 11:10:38 +0300

    Fix / fixed `DEBUG` in the `settings.py`

--- blogioma/settings.py
+++ blogioma/settings.py
@@ -18,7 +18,7 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don"t run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", False)
+DEBUG = bool(os.getenv("DEBUG", False))
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0"]
 INTERNAL_IPS = ["127.0.0.1", "localhost", "0.0.0.0"]
