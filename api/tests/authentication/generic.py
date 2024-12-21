3590e4d5eeecb8a51be15db7e1bc7ac3a19ca8d4
tiomchik <131615418+tiomchik@users.noreply.github.com>
2024-12-21 12:05:08 +0300

    Refactoring / reduced number of arguments of the `_set_social_links` method

--- api/tests/authentication/generic.py
+++ api/tests/authentication/generic.py
@@ -6,18 +6,8 @@ from main.utils import GenericTestCase
 
 
 class AuthenticationGenericTestCase(GenericTestCase):
-    def _set_social_links(
-        self,
-        youtube: str = "https://www.youtube.com/",
-        tiktok: str = "https://tiktok.com/",
-        twitch: str = "https://twitch.tv/",
-        linkedin: str = "https://linkedin.com/"
-    ) -> HttpResponse:
+    def _set_social_links(self, data: dict) -> HttpResponse:
         url = reverse("edit-me")
-        data = {
-            "youtube": youtube, "tiktok": tiktok,
-            "twitch": twitch, "linkedin": linkedin
-        }
         r = self.client.put(url, data, headers=self.authorization_header)
 
         self.user.refresh_from_db()
--- api/tests/authentication/test_social_links.py
+++ api/tests/authentication/test_social_links.py
@@ -14,7 +14,7 @@ class SocialLinksTests(AuthenticationGenericTestCase):
     }
 
     def test_set_social_links(self) -> None:
-        r = self._set_social_links(**self.social_links_data)
+        r = self._set_social_links(self.social_links_data)
 
         self.assertEqual(r.status_code, status.HTTP_200_OK)
         self.assertContains(r, self.social_links_data["youtube"])
@@ -23,7 +23,7 @@ class SocialLinksTests(AuthenticationGenericTestCase):
         self.assertContains(r, self.social_links_data["linkedin"])
 
     def test_read_social_links(self) -> None:
-        self._set_social_links()
+        self._set_social_links(self.social_links_data)
         url = reverse("me")
         r = self.client.get(url, headers=self.authorization_header)
 
@@ -33,16 +33,19 @@ class SocialLinksTests(AuthenticationGenericTestCase):
         self.assertContains(r, self.user.linkedin)
 
     def test_update_social_links(self) -> None:
-        self._set_social_links()
+        self._set_social_links(self.social_links_data)
         youtube = "https://www.youtube.com/@test"
-        r = self._set_social_links(youtube=youtube)
+        r = self._set_social_links({"youtube": youtube})
         self.assertContains(r, youtube)
 
     def test_delete_social_links(self) -> None:
-        self._set_social_links()
-        r = self._set_social_links(
-            youtube="", tiktok="", twitch="", linkedin=""
-        )
+        self._set_social_links(self.social_links_data)
+        r = self._set_social_links({
+            "youtube": "",
+            "tiktok": "",
+            "twitch": "",
+            "linkedin": "",
+        })
 
         self.assertEqual(r.json().get("youtube"), "")
         self.assertEqual(r.json().get("tiktok"), "")
