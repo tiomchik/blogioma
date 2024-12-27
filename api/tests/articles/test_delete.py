82294b585bc2d97fcd966bc855add58a61745354
tiomchik <131615418+tiomchik@users.noreply.github.com>
2024-12-27 16:16:53 +0300

    Refactoring / initialize `url` in `setUp` method in some tests

--- api/tests/articles/test_delete.py
+++ api/tests/articles/test_delete.py
@@ -5,13 +5,15 @@ from .generic import ArticleGenericTestCase
 
 
 class DeleteArticleTests(ArticleGenericTestCase):
+    def setUp(self):
+        super().setUp()
+        self.url = reverse("article-detail", kwargs={"pk": self.article.pk})
+
     def test_delete(self) -> None:
-        url = self._get_article_detail_url()
-        r = self.client.delete(url, headers=self.auth_header)
+        r = self.client.delete(self.url, headers=self.auth_header)
         self.assertEqual(r.status_code, status.HTTP_204_NO_CONTENT)
 
-        r = self.client.get(url)
-
+        r = self.client.get(self.url)
         self.assertEqual(r.status_code, status.HTTP_404_NOT_FOUND)
         self.assertEqual(
             r.json().get("detail"), "No Article matches the given query."
@@ -24,10 +26,5 @@ class DeleteArticleTests(ArticleGenericTestCase):
             "email": "test3@test.com"
         }
         self._set_another_user(**another_user_data)
-
-        url = self._get_article_detail_url()
-        r = self.client.delete(url, headers=self.auth_header)
+        r = self.client.delete(self.url, headers=self.auth_header)
         self._assert_forbidden_response(r)
-
-    def _get_article_detail_url(self) -> str:
-        return reverse("article-detail", kwargs={"pk": self.article.pk})
--- api/tests/articles/test_report.py
+++ api/tests/articles/test_report.py
@@ -8,21 +8,19 @@ from .generic import ArticleGenericTestCase
 class ReportArticleTests(ArticleGenericTestCase):
     report_data = {"reason": "Scam"}
 
-    def test_report(self) -> None:
-        url = self._get_report_article_url()
-        r = self.client.post(
-            url, self.report_data, headers=self.auth_header
-        )
+    def setUp(self):
+        super().setUp()
+        self.url = reverse("report-article", kwargs={"pk": self.article.pk})
 
+    def test_report(self) -> None:
+        r = self.client.post(
+            self.url, self.report_data, headers=self.auth_header
+        )
         self.assertEqual(r.status_code, status.HTTP_201_CREATED)
+
         report = Report.objects.get(pk=r.json().get("id"))
         self.assertIsNotNone(self.article.reports.get(pk=report.pk))
 
     def test_unauth_report(self) -> None:
-        url = self._get_report_article_url()
-        r = self.client.post(url, self.report_data)
-
+        r = self.client.post(self.url, self.report_data)
         self._check_unauth_response(r)
-
-    def _get_report_article_url(self) -> str:
-        return reverse("report-article", kwargs={"pk": self.article.pk})
