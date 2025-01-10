6320012e3236bcde70e59146264167a534fca24f
tiomchik <131615418+tiomchik@users.noreply.github.com>
2025-01-10 11:01:53 +0300

    Refactoring / create `is_author_or_staff` function

--- articles/views/delete.py
+++ articles/views/delete.py
@@ -6,6 +6,7 @@ from django.http import (
 from django.shortcuts import redirect
 
 from articles.models import Article
+from authentication.models import User
 
 
 def delete_article(
@@ -13,7 +14,11 @@ def delete_article(
 ) -> HttpResponseRedirect | HttpResponsePermanentRedirect:
     article = Article.objects.get(pk=pk)
 
-    if request.user == article.author or request.user.is_staff:
+    if is_author_or_staff(request.user, article):
         article.delete()
 
     return redirect("home")
+
+
+def is_author_or_staff(user: User, article: Article) -> bool:
+    return user == article.author or user.is_staff
