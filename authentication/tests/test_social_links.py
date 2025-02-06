2b434904ea7e826920d7183543ccc4af8f674c27
tiomchik <131615418+tiomchik@users.noreply.github.com>
2025-02-06 19:35:41 +0300

    Refactoring / use `for` cycle and rename `form_data` to `social_links`

--- authentication/tests/test_social_links.py
+++ authentication/tests/test_social_links.py
@@ -1,12 +1,11 @@
 from django.core.cache import cache
 from django.urls import reverse
-from urllib.parse import urlencode
 
 from .generic import AuthenticationGenericTestCase
 
 
 class SocialLinksTests(AuthenticationGenericTestCase):
-    form_data = {
+    social_links = {
         "youtube": "https://www.youtube.com/",
         "tiktok": "https://tiktok.com/",
         "twitch": "https://twitch.tv/",
@@ -14,28 +13,25 @@ class SocialLinksTests(AuthenticationGenericTestCase):
     }
 
     def test_set_social_links(self) -> None:
-        r = self.set_and_refresh_social_links(self.form_data)
-
-        self.assertEqual(r.status_code, 200)
-        self.assertEqual(self.user.youtube, self.form_data["youtube"])
-        self.assertEqual(self.user.tiktok, self.form_data["tiktok"])
-        self.assertEqual(self.user.twitch, self.form_data["twitch"])
-        self.assertEqual(self.user.linkedin, self.form_data["linkedin"])
+        r = self.set_and_refresh_social_links(self.social_links)
+        self.assertOkStatus(r)
+        for field in self.social_links.keys():
+            self.assertEqual(
+                getattr(self.user, field), self.social_links[field]
+            )
 
     def test_read_social_links(self) -> None:
-        self.set_and_refresh_social_links(self.form_data)
+        self.set_and_refresh_social_links(self.social_links)
 
         url = reverse("see_profile", kwargs={"username": self.user.username})
         cache.clear()
         r = self.client.get(url)
 
-        self.assertIn(self.user.youtube, str(r.content))
-        self.assertIn(self.user.tiktok, str(r.content))
-        self.assertIn(self.user.twitch, str(r.content))
-        self.assertIn(self.user.linkedin, str(r.content))
+        for field in self.social_links.keys():
+            self.assertContains(r, getattr(self.user, field))
 
     def test_update_social_links(self) -> None:
-        self.set_and_refresh_social_links(self.form_data)
+        self.set_and_refresh_social_links(self.social_links)
 
         youtube = "https://www.youtube.com/@test"
         self.set_and_refresh_social_links({"youtube": youtube})
@@ -43,15 +39,12 @@ class SocialLinksTests(AuthenticationGenericTestCase):
         self.assertEqual(self.user.youtube, youtube)
 
     def test_delete_social_links(self) -> None:
-        self.set_and_refresh_social_links(self.form_data)
+        self.set_and_refresh_social_links(self.social_links)
         self.set_and_refresh_social_links({
             "youtube": "",
             "tiktok": "",
             "twitch": "",
             "linkedin": ""
         })
-
-        self.assertEqual(self.user.youtube, "")
-        self.assertEqual(self.user.tiktok, "")
-        self.assertEqual(self.user.twitch, "")
-        self.assertEqual(self.user.linkedin, "")
+        for field in self.social_links.keys():
+            self.assertEqual(getattr(self.user, field), "")
