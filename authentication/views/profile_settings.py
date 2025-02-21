afbe1b9836f4f0e3fcf99a62205b073b6b4ddced
tiomchik <131615418+tiomchik@users.noreply.github.com>
2025-02-21 15:50:56 +0300

    Refactoring / move the author authentication to the template

--- authentication/views/profile_settings.py
+++ authentication/views/profile_settings.py
@@ -9,14 +9,8 @@ from main.utils import get_base_context
 @cache_page(60 * 30)
 def profile_settings(request: HttpRequest, username: str) -> HttpResponse:
     user = get_object_or_404(User, username=username)
-
-    valid = 0
-    if request.user.is_authenticated and request.user.username == username:
-        valid = 1
-
     context = get_base_context(
         request, name=f"{username}'s profile settings",
-        profile=user, valid=valid
+        profile=user
     )
-
     return render(request, "profile/settings.html", context)
