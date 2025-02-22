ebf1904675a64229b94534e456bf64786196c858
tiomchik <131615418+tiomchik@users.noreply.github.com>
2025-02-22 19:02:44 +0300

    Refactoring / create the `get_articles_by_authors_name` function

--- authentication/views/profile.py
+++ authentication/views/profile.py
@@ -9,15 +9,19 @@ from main.utils import get_paginator_context
 
 @cache_page(30)
 def profile(request: HttpRequest, username: str) -> HttpResponse:
-    articles = Article.objects.filter(author__username=username).values(
-        "heading", "full_text", "update", 
-        "pub_date", "pk", "author", "author__pfp", 
-        "author__username"
-    )
-
+    articles = get_articles_by_authors_name(username)
     context = get_paginator_context(
         request, articles, f"{username}'s profile",
         profile=get_object_or_404(User, username=username)
     )
-
     return render(request, "profile/profile.html", context)
+
+
+def get_articles_by_authors_name(
+    author_name: str
+) -> list[Article]:
+    return Article.objects.filter(author__username=author_name).values(
+        "heading", "full_text", "update", 
+        "pub_date", "pk", "author",
+        "author__pfp", "author__username"
+    )
