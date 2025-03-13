e7b66935bd8832eb69d6418e8cc0be5e8a1ce994
tiomchik <131615418+tiomchik@users.noreply.github.com>
2025-03-13 15:29:06 +0300

    Refactoring / create the `get_comments_by_article` function

--- comments/views/get.py
+++ comments/views/get.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.views.decorators.cache import cache_page
 
 from articles.utils import get_article_by_pk
+from articles.models import Article
 from main.utils import get_paginator_context
 from comments.models import Comment
 
@@ -10,15 +11,17 @@ from comments.models import Comment
 @cache_page(30)
 def get_comments(request: HttpRequest, pk: int) -> HttpResponse:
     article = get_article_by_pk(pk)
-    comments = Comment.objects.filter(article=article).values(
-        "author", "author__pfp", "pk", "article__pk",
-        "author__username", "update", "pub_date", "text"
-    )
-
+    comments = get_comments_by_article(article)
     context = get_paginator_context(
         request, object_list=comments,
         name=f"Comments to article \"{article.heading}\".",
         article=article
     )
-
     return render(request, "comments/comments.html", context)
+
+
+def get_comments_by_article(article: Article) -> list[Comment]:
+    return Comment.objects.filter(article=article).values(
+        "author", "author__pfp", "pk", "article__pk",
+        "author__username", "update", "pub_date", "text"
+    )
