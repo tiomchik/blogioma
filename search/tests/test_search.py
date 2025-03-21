efb79cb1d8d87c3a793dd3fc2bfea608d21ee9d7
tiomchik <131615418+tiomchik@users.noreply.github.com>
2025-03-21 18:51:49 +0300

    Refactoring / use `self.article` in tests

--- search/tests/test_search.py
+++ search/tests/test_search.py
@@ -3,27 +3,17 @@ from .generic import SearchGenericTestCase
 
 class SearchTests(SearchGenericTestCase):
     def test_search(self) -> None:
-        article = self.create_article(heading="find_me")
-
-        r = self.search_article(article.heading)
+        r = self.search_article(self.article.heading)
 
         # count=3: 1st in the title tag, 2nd in the main page heading
         # (Search results by query: "find_me") and 3rd in the article card
-        self.assertContains(r, article.heading, count=3)
+        self.assertContains(r, self.article.heading, count=3)
 
     def test_search_by_author(self) -> None:
-        article = self.create_article(heading="my new article")
-
         r = self.search_article(self.user.username)
-
         self.assertContains(r, self.article.heading)
-        self.assertContains(r, article.heading)
 
     def test_search_by_part_of_article_heading(self) -> None:
-        heading = "nicest article"
-        self.create_article(heading=heading)
-
-        query = heading[:5]
+        query = self.article.heading[:5]
         r = self.search_article(query)
-
-        self.assertContains(r, heading)
+        self.assertContains(r, self.article.heading)
