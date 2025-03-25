05f3526fcb5c32c2f503c11143fbed42766212be
tiomchik <131615418+tiomchik@users.noreply.github.com>
2025-03-25 10:07:54 +0300

    Refactoring / typify template tags

--- main/templatetags/main_tags.py
+++ main/templatetags/main_tags.py
@@ -1,26 +1,28 @@
 from django import template
 from django.core.paginator import Page
+from django.forms import Form
 
 from articles.utils import get_articles_ordered_by_field
+
 
 register = template.Library()
 
 
 @register.inclusion_tag("main/tags/for_articles.html")
-def show_page_obj_articles(page_obj: Page):
+def show_page_obj_articles(page_obj: Page) -> dict:
     return {"articles_for": page_obj}
 
 
 @register.inclusion_tag("main/tags/for_articles.html")
-def show_6_articles(order_by: str):
+def show_6_articles(order_by: str) -> dict:
     return {"articles_for": get_articles_ordered_by_field(order_by)[:6]}
 
 
 @register.inclusion_tag("main/tags/form.html")
-def show_form(form):
+def show_form(form: Form) -> dict:
     return {"form": form}
 
 
 @register.inclusion_tag("main/tags/paginator.html")
-def paginator(page_obj):
+def paginator(page_obj: Page) -> dict:
     return {"page_obj": page_obj}
