460dfdfeeb1405d201ca9b5a17e89be48eaf7722
tiomchik <131615418+tiomchik@users.noreply.github.com>
2025-03-27 08:15:36 +0300

    Refactoring / remove duplicate test

--- articles/tests/test_read.py
+++ articles/tests/test_read.py
@@ -4,11 +4,6 @@ from .generic import ArticleGenericTestCase
 
 
 class ReadArticleTests(ArticleGenericTestCase):
-    def test_read_at_home_page(self) -> None:
-        url = reverse("home")
-        r = self.client.get(url)
-        self.assertPageContainsArticle(r, self.article)
-
     def test_read_detail(self) -> None:
         url = reverse("read", kwargs={"pk": self.article.pk})
         r = self.client.get(url)
