815e909e4d4339bc71d2e32212da50b0e13c8193
tiomchik <131615418+tiomchik@users.noreply.github.com>
2025-03-28 09:24:51 +0300

    Innovation / implement `order_by` functionality in article list view

--- api/views/article/list.py
+++ api/views/article/list.py
@@ -4,8 +4,22 @@ from rest_framework.mixins import ListModelMixin
 from rest_framework.response import Response
 from rest_framework.request import Request
 
+from articles.models import Article
+
 
 class ListArticleMixin(ListModelMixin):
     @method_decorator(cache_page(30))
     def list(self, request: Request) -> Response:
-        return super().list(request)
+        order_by = request.query_params.get("order_by")
+        if order_by:
+            queryset = Article.objects.order_by(order_by)
+        else:
+            queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
