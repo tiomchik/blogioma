ae8b8dd2182716db45cf4c7bbf1f1d8747561164
tiomchik <131615418+tiomchik@users.noreply.github.com>
2025-07-04 09:38:27 +0300

    Innovation / add `AuthWrapper` to the root render

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,12 @@
-import { StrictMode } from "react";
+import { JSX, StrictMode, useState } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@/app/styles/index.scss";
 
 import { routeTree } from "./routeTree.gen";
-import { AuthContext } from "./app/contexts";
+import { AuthContext } from "@/app/contexts";
+import { User } from "@/app/types";
 
 const router = createRouter({ routeTree });
 
@@ -17,16 +18,31 @@ declare module "@tanstack/react-router" {
 
 const queryClient = new QueryClient();
 
+const AuthWrapper = ({
+  children,
+  user,
+}: {
+  children: JSX.Element;
+  user: User | null;
+}): JSX.Element => {
+  const [currentUser, setCurrentUser] = useState<User | null>(user);
+  return (
+    <AuthContext value={{ currentUser, setCurrentUser }}>
+      {children}
+    </AuthContext>
+  );
+};
+
 const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <AuthContext value={{ user: null }}>
+      <AuthWrapper user={null}>
         <QueryClientProvider client={queryClient}>
           <RouterProvider router={router} />
         </QueryClientProvider>
-      </AuthContext>
+      </AuthWrapper>
     </StrictMode>
   );
 }
