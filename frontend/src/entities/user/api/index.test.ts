9044807d4334143774fe980047140345541b370e
tiomchik <131615418+tiomchik@users.noreply.github.com>
2025-07-04 10:48:50 +0300

    Innovation / write tests for user API

---
+++ frontend/src/entities/user/api/index.test.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { describe, expect, MockedFunction, test, vi } from "vitest";
+import { createUser, obtainToken } from "./";
+import { createDummyFile } from "@/tests/utils";
+
+vi.mock("axios", () => {
+  return {
+    default: {
+      post: vi.fn(),
+    },
+  };
+});
+
+const mockedAxiosPost = vi.mocked(axios.post) as MockedFunction<
+  typeof axios.post
+>;
+
+describe("createUser", () => {
+  test("axios.post was called with the correct arguments", async () => {
+    const formData = new FormData();
+    formData.append("username", "username");
+    formData.append("password", "12341234");
+    formData.append("email", "test@example.com");
+    formData.append("pfp", createDummyFile("pfp.png", "image/png"));
+    await createUser(formData);
+    expect(mockedAxiosPost).toHaveBeenCalledWith(
+      `${import.meta.env.VITE_API_URL}/auth/register/`,
+      formData
+    );
+  });
+});
+
+describe("obtainToken", () => {
+  test("axios.post was called with the correct arguments", async () => {
+    mockedAxiosPost.mockResolvedValueOnce({ data: { token: "token" } });
+    await obtainToken("username", "password");
+    expect(mockedAxiosPost).toHaveBeenCalledWith(
+      `${import.meta.env.VITE_API_URL}/auth/obtain-token/`,
+      { username: "username", password: "password" }
+    );
+  });
+});
