37b7217b0fc2d7c65f23a7645a87f3833e3a4d52
tiomchik <131615418+tiomchik@users.noreply.github.com>
2025-07-04 09:56:25 +0300

    Refactoring / replace `fetch` to the `axios.get`

--- frontend/src/entities/article/api/index.ts
+++ frontend/src/entities/article/api/index.ts
@@ -1,11 +1,13 @@
+import axios from "axios";
+
 const loadArticlesOrderedByField = async (
   field: string,
   amount?: number
 ): Promise<Response> => {
-  const response = await fetch(
+  const response = await axios.get(
     `${import.meta.env.VITE_API_URL}/articles/?order_by=${field}&page_size=${amount ? amount : ""}`
   );
-  return response.json();
+  return response.data;
 };
 
 export { loadArticlesOrderedByField };
