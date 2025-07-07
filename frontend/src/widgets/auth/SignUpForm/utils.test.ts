3e7eb9a6d8c00048dcfc95d7c2df2c9872ecf810
tiomchik <131615418+tiomchik@users.noreply.github.com>
2025-07-07 09:30:01 +0300

    Innovation / write tests for the `utils.ts`

---
+++ frontend/src/widgets/auth/SignUpForm/utils.test.ts
@@ -0,0 +1,100 @@
+import { describe, expect, test, vi } from "vitest";
+import {
+  createAndPopulateFormData,
+  handleOnSuccess,
+  setErrorsFromResponse,
+} from "./utils";
+import { setAuthToken } from "@/entities/user/api";
+import { AxiosError } from "axios";
+
+vi.mock("axios", () => {
+  return {
+    default: {
+      get: vi.fn(),
+      post: vi.fn(),
+      defaults: {
+        headers: {
+          common: {},
+        },
+      },
+    },
+  };
+});
+
+vi.mock("@/entities/user/api", async () => {
+  const actual = await vi.importActual("@/entities/user/api");
+  return {
+    ...actual,
+    setAuthToken: vi.fn(),
+    obtainToken: vi.fn(() => "token"),
+  };
+});
+
+const mockedSetAuthToken = vi.mocked(setAuthToken);
+
+const data = {
+  username: "username",
+  password: "password",
+  password1: "password",
+};
+
+describe("createAndPopulateFormData", () => {
+  test("returns a FormData object with the correct data", () => {
+    const formData = createAndPopulateFormData(data);
+    expect(formData.get("username")).toBe(data.username);
+    expect(formData.get("password")).toBe(data.password);
+    expect(formData.get("password1")).toBe(data.password1);
+    expect(formData.get("pfp")).toBeNull();
+    expect(formData.get("email")).toBeNull();
+  });
+});
+
+describe("handleOnSuccess", () => {
+  test("all functions were called correctly", async () => {
+    const mockedSetCurrentUser = vi.fn();
+    const mockedNavigate = vi.fn();
+    await handleOnSuccess(data, mockedSetCurrentUser, mockedNavigate);
+    expect(mockedSetAuthToken).toHaveBeenCalledWith("token");
+    expect(mockedSetCurrentUser).toHaveBeenCalledWith({
+      username: data.username,
+      pfp: undefined,
+    });
+    expect(mockedNavigate).toHaveBeenCalledWith({ to: "/" });
+  });
+});
+
+describe("setErrorsFromResponse", () => {
+  const mockedSetError = vi.fn();
+
+  test("sets field errors correctly", () => {
+    const error = {
+      request: {
+        response: JSON.stringify({
+          username: ["This field is required."],
+          password: ["This field is required."],
+        }),
+      },
+    } as AxiosError;
+
+    setErrorsFromResponse(error, mockedSetError);
+
+    expect(mockedSetError).toHaveBeenCalledWith("username", {
+      message: "This field is required.",
+    });
+    expect(mockedSetError).toHaveBeenCalledWith("password", {
+      message: "This field is required.",
+    });
+  });
+
+  test("sets server error correctly", () => {
+    const error = {
+      request: {
+        response: JSON.stringify({ detail: "error from the server" }),
+      },
+    } as AxiosError;
+    setErrorsFromResponse(error, mockedSetError);
+    expect(mockedSetError).toHaveBeenCalledWith("root", {
+      message: "error from the server",
+    });
+  });
+});
