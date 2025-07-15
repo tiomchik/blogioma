b2044f84b46407d203c3c1f7245dcd06edd4a717
tiomchik <131615418+tiomchik@users.noreply.github.com>
2025-07-15 12:28:02 +0300

    Innovation / add the `page_amount` field to the pagination response

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -1,7 +1,17 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 
 class Pagination(PageNumberPagination):
     page_size = 50
     page_size_query_param = "page_size"
     max_page_size = 100
+
+    def get_paginated_response(self, data) -> Response:
+        return Response({
+            "count": self.page.paginator.count,
+            "page_amount": self.page.paginator.num_pages,
+            "next": self.get_next_link(),
+            "previous": self.get_previous_link(),
+            "results": data
+        })
