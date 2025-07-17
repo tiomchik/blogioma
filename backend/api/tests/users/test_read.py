ef52ef1bd0808e55144273018aa2922c6f509c58
tiomchik <131615418+tiomchik@users.noreply.github.com>
2025-07-17 09:21:53 +0300

    Innovation / write the `test_read_non_existent_user`

--- backend/api/tests/users/test_read.py
+++ backend/api/tests/users/test_read.py
@@ -1,11 +1,21 @@
+from django.http import HttpResponse
 from django.urls import reverse
 from main.generic_test_cases.api import APIGenericTestCase
 
 
 class ReadUserTests(APIGenericTestCase):
     def test_read_detail(self) -> None:
-        url = reverse("user-detail", kwargs={"username": self.user.username})
+        r = self.get_user_detail(self.user.username)
+        json_data = r.json()
+        self.assertEqual(json_data.get("id"), self.user.id)
+        self.assertEqual(json_data.get("username"), self.user.username)
+        self.assertEqual(json_data.get("email"), self.user.email)
+
+    def test_read_non_existent_user(self) -> None:
+        r = self.get_user_detail("non_existent_user")
+        self.assertEqual(r.status_code, 404)
+
+    def get_user_detail(self, username: str) -> HttpResponse:
+        url = reverse("user-detail", kwargs={"username": username})
         r = self.client.get(url)
-        self.assertEqual(r.json().get("id"), self.user.id)
-        self.assertEqual(r.json().get("username"), self.user.username)
-        self.assertEqual(r.json().get("email"), self.user.email)
+        return r
