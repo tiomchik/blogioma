e974ca4f7666c1cdea07307a4cc0e5602b7db89c
tiomchik <131615418+tiomchik@users.noreply.github.com>
2025-10-29 11:01:01 +0300

    Fix / typize the search in the `/articles` route correctly

--- frontend/src/app/routes/articles.tsx
+++ frontend/src/app/routes/articles.tsx
@@ -1,16 +1,16 @@
 import ArticlesPage from "@/pages/articles";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, SearchSchemaInput } from "@tanstack/react-router";
 
 export type ArticleSortingCriterias = "popular" | "latest";
 
 type ArticleSearch = {
   sortingCriteria: ArticleSortingCriterias;
-  page: number;
-};
+  page?: number;
+} & SearchSchemaInput;
 
 export const Route = createFileRoute("/articles")({
   component: ArticlesPage,
-  validateSearch: (search): ArticleSearch => {
+  validateSearch: (search: ArticleSearch) => {
     return {
       sortingCriteria: search.sortingCriteria as ArticleSortingCriterias,
       page: search.page ? Number(search.page) : 1,
